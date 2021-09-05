4bd521261a34eaf9b6a7a877166044eb891a2b85
vivek <vivek.reck@gmail.com>
2021-09-05 17:32:26 +0530

    adding color and bootcamp model

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const bootcamps = require('./routes/bootcamps.routes');
+const colors = require('colors');
+const connectDB = require('./configs/db')
 
 // Load env vars
 dotenv.config({ path: './configs/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps.routes');
 
 const app = express();
 
@@ -14,7 +19,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
