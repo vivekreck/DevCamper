ec857239f74cccbfc061ac01ff613d5a938a3363
vivek <vivek.reck@gmail.com>
2021-09-25 09:21:18 +0530

    adding sort and select

--- controllers/bootcamps.controllers.js
+++ controllers/bootcamps.controllers.js
@@ -10,12 +10,39 @@ const asyncHandler = require('../middleware/async');
 // @route       GET /api/v1/bootcamps
 // @access      Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    // Copy req.query
+    const reqQuery = { ...req.query };
+
+    // Fields to exclude
+    const removeFields = ['select'];
+
+    // loop over removeFields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param]);
+
+    // Create query string
     let queryStr = JSON.stringify(req.query);
 
+    // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
+    // Finding resource
     let query = Bootcamp.find(JSON.parse(queryStr));
 
+    // Select Fields
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sort 
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    // executing query
     const bootcamps = await query;
 
     res
